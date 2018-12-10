ffdb43e473acb6572e6fc5f4120e2e99d4fa89e4
billotei <ateilhet@gmail.com>
2018-12-10 10:28:20 +0100

    day6 1 fix manhatan formula

--- src/main/scala/days/Day6.scala
+++ src/main/scala/days/Day6.scala
@@ -36,20 +36,7 @@ object Day6 {
       //.foreach(println)
   }
 
-  private def manhatan(a: (Int, Int), b: (Int, Int)) = Math.abs(a._1 - b._2) + Math.abs(a._2 - b._2)
-
-
-
-
-
-
-
-
-
-
-
-
-
+  private def manhatan(a: (Int, Int), b: (Int, Int)) = Math.abs(a._1 - b._1) + Math.abs(a._2 - b._2)
 
   def part2() = {
 
