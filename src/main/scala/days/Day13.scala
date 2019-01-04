29ae05a6bcdfb971ffc1d4997e501b070c973181
billotei <ateilhet@gmail.com>
2019-01-04 12:21:45 +0100

    day13 wip

--- src/main/scala/days/Day13.scala
+++ src/main/scala/days/Day13.scala
@@ -2,7 +2,6 @@ package days
 
 import scala.collection.mutable.ArrayBuffer
 import scala.io.Source
-import scala.util.Try
 
 object Day13 {
   private val carts =
@@ -18,17 +17,20 @@ object Day13 {
           l._1
             .split("")
             .zipWithIndex
-            .filter(_._1 != " ")
+            //.filter(_._1 != " ")
             .map(p => {
-              if (List("<", ">", "^", "v").contains(p._1)) {
-                carts.+=((p._2, l._2, p._1, ("l", "s", "r")))
+              p._1 match {
+                case _ @ ">" | "<" =>
+                  carts.+=((p._2, l._2, p._1, ("l", "s", "r")))
+                  (p._2, l._2, "-")
+                case _ @ "v" | "^" =>
+                  carts.+=((p._2, l._2, p._1, ("l", "s", "r")))
+                  (p._2, l._2, "|")
+                case _ => (p._2, l._2, p._1)
               }
-
-              (p._2, l._2, p._1)
             }))
       .toArray
   }
-  //.foreach(l => l.foreach(println))
 
   private def process(p: (Int, Int, String),
                       c: (Int, Int, String, (String, String, String))) =
@@ -74,23 +76,24 @@ object Day13 {
     case "^" => process(source(c._2 - 1)(c._1), c)
   }
 
-  def part1() = {
+  def part1(): (Int, Int) = {
     val s = source()
     def go(source: Array[Array[(Int, Int, String)]],
-           carts: ArrayBuffer[(Int, Int, String, (String, String, String))])
-      : (Int, Int) = {
+           carts: ArrayBuffer[(Int, Int, String, (String, String, String))],
+           i: Int): (Int, Int) = {
       val cartsByPos = carts.map(c => ((c._1, c._2), c._3)).groupBy(_._1)
       if (cartsByPos.exists(_._2.length > 1)) {
         cartsByPos.find(_._2.length > 1).get._1
       } else {
         go(
           source,
-          carts.map(c => move(c, source))
+          carts.map(c => move(c, source)),
+          i + 1
         )
       }
     }
 
-    go(s, carts)
+    go(s, carts, 0)
   }
 
   def part2() = {}
