6bab368f8c153d3d7a5c1017091223bb41f96487
ateilhet <ateilhet@gmail.com>
2019-01-11 17:54:11 +0100

    day15 wip

--- src/main/scala/days/Day15.scala
+++ src/main/scala/days/Day15.scala
@@ -4,14 +4,14 @@ import scala.collection.mutable.ArrayBuffer
 import scala.io.Source
 
 object Day15 {
-  val fighters: ArrayBuffer[((Int, Int), String)] = ArrayBuffer()
+  val fightersArr: ArrayBuffer[((Int, Int), String)] = ArrayBuffer()
   val map: Array[((Int, Int), String)] = Source.fromFile("sources/source15.txt").getLines
     .zipWithIndex
     .flatMap(l => l._1
       .split("")
       .zipWithIndex
       .map(p => {
-        if (List("G", "E").contains(p._1)) fighters.+=(((p._2, l._2), p._1))
+        if (List("G", "E").contains(p._1)) fightersArr.+=(((p._2, l._2), p._1))
 
         ((p._2, l._2), p._1)
       })
@@ -20,9 +20,13 @@ object Day15 {
 
   def part1() = {
     val m = map
-    val frs = fighters.sortBy(f => (f._1._2, f._1._1))
+    val frs = fightersArr.sortBy(f => (f._1._2, f._1._1))
 
-    frs.foreach(println)
+    def go(fighters: ArrayBuffer[((Int, Int), String)], i: Int) = {
+      val f = fighters(i)
+      val opponents = fighters.filter(_._2 != f._2)
+
+    }
   }
 
   def part2() = {
