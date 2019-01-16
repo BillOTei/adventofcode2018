cc781aa51e2f6da60d93e1195e6deb142694cf14
ateilhet <ateilhet@gmail.com>
2019-01-16 12:55:31 +0100

    day15 breadth first search

--- src/main/scala/days/Day15.scala
+++ src/main/scala/days/Day15.scala
@@ -5,17 +5,21 @@ import scala.io.Source
 
 object Day15 {
   val fightersArr: ArrayBuffer[((Int, Int), String)] = ArrayBuffer()
-  val map: Array[((Int, Int), String)] = Source.fromFile("sources/source15.txt").getLines
+  val map: Array[((Int, Int), String)] = Source
+    .fromFile("sources/source15.txt")
+    .getLines
     .zipWithIndex
-    .flatMap(l => l._1
-      .split("")
-      .zipWithIndex
-      .map(p => {
-        if (List("G", "E").contains(p._1)) fightersArr.+=(((p._2, l._2), p._1))
+    .flatMap(
+      l =>
+        l._1
+          .split("")
+          .zipWithIndex
+          .map(p => {
+            if (List("G", "E").contains(p._1))
+              fightersArr.+=(((p._2, l._2), p._1))
 
-        ((p._2, l._2), p._1)
-      })
-    )
+            ((p._2, l._2), p._1)
+          }))
     .toArray
 
   def part1() = {
@@ -25,7 +29,13 @@ object Day15 {
     def go(fighters: ArrayBuffer[((Int, Int), String)], i: Int) = {
       val f = fighters(i)
       val opponents = fighters.filter(_._2 != f._2)
-      val inRangeReachable = opponents.flatMap(o => getInRange(m, o))
+      val inRangeReachable = opponents
+        .flatMap(o => getInRange(m, o))
+        .filter(p => {
+          val t = bfs(m, f, p)
+
+          true
+        })
 
       1
     }
@@ -33,9 +43,37 @@ object Day15 {
     go(frs, 0)
   }
 
-  private def path(map: Array[((Int, Int), String)], a: ((Int, Int), String), b: ((Int, Int), String))
+  private def bfs(map: Array[((Int, Int), String)],
+                  start: ((Int, Int), String),
+                  end: ((Int, Int), String)) = {
+    def go(q: Array[((Int, Int), String)],
+           m: Array[((Int, Int), String)]): Array[((Int, Int), String)] = {
 
-  private def getInRange(map: Array[((Int, Int), String)], p: ((Int, Int), String)) = {
+      val remaining = ArrayBuffer[((Int, Int), String)]()
+      val surroundings = q.flatMap(node => {
+        val (s, r) = m.partition(p => {
+          val (x, y) = node._1
+          List((x, y - 1), (x, y + 1), (x + 1, y), (x - 1, y))
+            .contains(p._1) && p._2 == "."
+        })
+
+        remaining.++=(r)
+
+        s
+      })
+
+      if (surroundings.contains(end)) {
+        q ++ surroundings
+      } else {
+        go(q ++ surroundings, remaining.toArray)
+      }
+    }
+
+    go(Array(start), map)
+  }
+
+  private def getInRange(map: Array[((Int, Int), String)],
+                         p: ((Int, Int), String)) = {
     val (x, y) = p._1
     val surroundings = List((x, y - 1), (x, y + 1), (x + 1, y), (x - 1, y))
 
@@ -44,7 +82,5 @@ object Day15 {
       .sortBy(p => (p._1._2, p._1._1))
   }
 
-  def part2() = {
-
-  }
+  def part2() = {}
 }
