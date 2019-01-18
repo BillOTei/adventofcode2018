97a8ec1703ca5079f1f8de5f613c2849b9a3068d
billotei <ateilhet@gmail.com>
2019-01-18 17:16:20 +0100

    day15 wip breadth first search, closest opponent

--- src/main/scala/days/Day15.scala
+++ src/main/scala/days/Day15.scala
@@ -31,13 +31,16 @@ object Day15 {
       val opponents = fighters.filter(_._2 != f._2)
       val inRangeReachable = opponents
         .flatMap(o => getInRange(m, o))
-        .filter(p => {
-          val t = bfs(m, f, p)
+        .foldLeft(List[(((Int, Int), String), Int)]())((acc, p) => {
+          val count = bfs(m, f, p)
 
-          true
+          if (count != -1) {
+            acc :+ (p, count)
+          } else acc
         })
+        .sortBy(p => (p._2, p._1._1._2, p._1._1._1)) // Sort by path length then reading order (y then x)
 
-      1
+      inRangeReachable.head
     }
 
     go(frs, 0)
@@ -45,31 +48,45 @@ object Day15 {
 
   private def bfs(map: Array[((Int, Int), String)],
                   start: ((Int, Int), String),
-                  end: ((Int, Int), String)) = {
-    def go(q: Array[((Int, Int), String)],
-           m: Array[((Int, Int), String)]): Array[((Int, Int), String)] = {
+                  end: ((Int, Int), String)): Int = {
 
+    val freeCount = map.count(_._2 == ".")
+
+    def go(q: Array[((Int, Int), String)],
+           m: Array[((Int, Int), String)],
+           v: ArrayBuffer[((Int, Int), String)],
+           i: Int): Int = {
       val remaining = ArrayBuffer[((Int, Int), String)]()
-      val surroundings = q.flatMap(node => {
-        val (s, r) = m.partition(p => {
-          val (x, y) = node._1
-          List((x, y - 1), (x, y + 1), (x + 1, y), (x - 1, y))
-            .contains(p._1) && p._2 == "."
+      val localVisited = ArrayBuffer[((Int, Int), String)]()
+
+      val surroundings = q
+        .filterNot(n => v.contains(n))
+        .flatMap(node => {
+          val (s, r) = m.partition(p => {
+            val (x, y) = node._1
+            List((x, y - 1), (x, y + 1), (x + 1, y), (x - 1, y))
+              .contains(p._1) && p._2 == "."
+          })
+
+          remaining.++=(r)
+          localVisited.+=(node)
+
+          s
         })
 
-        remaining.++=(r)
-
-        s
-      })
-
       if (surroundings.contains(end)) {
-        q ++ surroundings
+        i + 1
+      } else if (v.size + localVisited.size == freeCount - 1) {
+        -1
       } else {
-        go(q ++ surroundings, remaining.toArray)
+        go((q ++ surroundings).distinct,
+           remaining.toArray,
+           v.++=(localVisited).distinct,
+           i + 1)
       }
     }
 
-    go(Array(start), map)
+    go(Array(start), map, ArrayBuffer.empty, 0)
   }
 
   private def getInRange(map: Array[((Int, Int), String)],
